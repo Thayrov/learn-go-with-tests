9b9f7baa5c75cb35b8448ad2f66adaaa2d8c5dc6
Thayrov <thayrovg@gmail.com>
2024-07-08 23:44:06 -0600

    refactor: Update Repeat function in iteration package to accept a variable number of repetitions

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,7 +1,7 @@
 package iteration
 
-func Repeat(char string) (repeated string) {
-	for i := 0; i < 5; i++ {
+func Repeat(char string, times int) (repeated string) {
+	for i := 0; i < times; i++ {
 		repeated += char
 	}
 	return
