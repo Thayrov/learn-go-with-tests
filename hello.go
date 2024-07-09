d00f467c327de9d1ad000f0e1b2f791b5015ef73
Thayrov <thayrovg@gmail.com>
2024-07-08 19:45:31 -0600

    refactor: Update Hello function to handle empty name parameter and support multiple languages

--- hello.go
+++ hello.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
-const salutation = "Hello, "
+const salutationEN = "Hello, "
+const salutationES = "Hola, "
 
-func Hello(name string) string {
+const es = "Spanish"
+const en = "English"
+
+func Hello(name string, lang string) string {
+	salutation := ""
 	if name == "" {
 		name = "World"
+	}
+	if lang == en || lang == "" {
+		salutation = salutationEN
+	}
+	if lang == es {
+		salutation = salutationES
 	}
 	return salutation + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("should change language depending on 2nd parameter", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
 		assertCorrectMessage(t, got, want)
 	})
 }
